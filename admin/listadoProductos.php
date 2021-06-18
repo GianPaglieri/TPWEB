3fc05e040b9ad7639f0264862c2240217e48565b
GianPaglieri <80928152+GianPaglieri@users.noreply.github.com>
2021-06-17 23:28:16 -0300

    CORRECIONES

--- admin/listadoProductos.php
+++ admin/listadoProductos.php
@@ -74,7 +74,7 @@ include ("includes/sidebar.php")
 									  <td><?php echo $prod ["categoria"] ?></td>
 									  <td><?php echo $prod ["marca"] ?></td>
                                       <td><?php echo $prod ["precio"] ?></td>
-                                      <td> <?php echo $prod ["activa"]? 'TRUE' :'FALSE' ?></td>
+                                      <td> <?php echo $prod ["activa"]? 'SI' :'NO' ?></td>
                                       <td> 
                                       <a href="ABM/formularioProducto.php?edit=<?php echo $prod['id']?>" class="btn btn-warning">Editar</a>
                                       <a href= "ListadoProductos.php?del=<?php echo $prod ['id']?>" class="btn btn-danger">Borrar</a>
