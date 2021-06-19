2a5e6231b4ddbcbb879c379d012b2a2847982731
GianPaglieri <80928152+GianPaglieri@users.noreply.github.com>
2021-06-19 16:52:41 -0300

    sidebar

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -14,7 +14,7 @@
 							</li>
 
 							<li>
-								<a href="message.php">
+								<a href="listadoComentarios.php">
 									<i class="menu-icon icon-inbox"></i>
 									Inbox
 									<b class="label green pull-right">11</b>
