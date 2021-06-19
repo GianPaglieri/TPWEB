3a2a967c562f932f771a537f4c3d6c3d928d5735
GianPaglieri <80928152+GianPaglieri@users.noreply.github.com>
2021-06-19 12:18:42 -0300

    correciones listado

--- admin/listadoProductos.php
+++ admin/listadoProductos.php
@@ -91,47 +91,7 @@ include ("includes/sidebar.php")
                         </div>
                         <!--/.module-->
                         <br />
-                        <div class="module">
-                            <div class="module-head">
-                                <h3>
-                                    Pie - Default</h3>
-                            </div>
-                            <div class="module-body">
-                                <div class="chart pie donut">
-                                    <div id="pie-default" class="graph">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!--/.module-->
-                        <br />
-                        <div class="module">
-                            <div class="module-head">
-                                <h3>
-                                    Pie - Donut</h3>
-                            </div>
-                            <div class="module-body">
-                                <div class="chart pie donut">
-                                    <div id="pie-donut" class="graph">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!--/.module-->
-                        <br />
-                        <div class="module">
-                            <div class="module-head">
-                                <h3>
-                                    Pie - Donut & Interactive</h3>
-                            </div>
-                            <div class="module-body">
-                                <div class="chart pie donut interactive">
-                                    <div id="pie-interactive" class="graph">
-                                    </div>
-                                    <div id="hover">
-                                    </div>
-                                </div>
-                            </div>
+                        
                         </div>
                         <!--/.module-->
                     </div>
